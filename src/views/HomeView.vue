3779a75a4c7d77d6f077a3ea668b168cb581e043
xnm-sdksdk <n.mendonca@protonmail.com>
2023-12-06 22:26:37 +0000

    home view with logic

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,36 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <i>{{ quote }}</i>
+    <p>Kanye West</p>
+  </div>
 </template>
+
+<script>
+import { ref, onMounted } from "vue";
+
+export default {
+  setup() {
+    const quote = ref("");
+
+    const apiUrl = "https://api.kanye.rest/";
+    onMounted(() => {
+      fetch(apiUrl)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          quote.value = data.quote;
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error.message);
+        });
+    });
+    return {
+      quote,
+    };
+  },
+};
+</script>

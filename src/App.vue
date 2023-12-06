5d4bbdbb7af6fd1decc04b6e4a2d75a7d52ed49b
xnm-sdksdk <n.mendonca@protonmail.com>
2023-12-06 22:25:36 +0000

    app vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <RouterView />
+  <router-view></router-view>
 </template>
 
-<style>
-</style>
+<style></style>
